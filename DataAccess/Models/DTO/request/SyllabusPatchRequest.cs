2b75cba31158e6b912f5714bec322a5dbca3b6c0
Nguyen Phong Hao <haonphe153346@fpt.edu.vn>
2023-10-26 10:12:47 +0700

    syllabus patch

--- DataAccess/Models/DTO/request/SyllabusPatchRequest.cs
+++ DataAccess/Models/DTO/request/SyllabusPatchRequest.cs
@@ -8,8 +8,6 @@ namespace DataAccess.Models.DTO.request
 {
     public class SyllabusPatchRequest
     {
-    
-
         public int? syllabus_id { get; set; }
         public string? syllabus_description { get; set; }
         public string? degree_level { get; set; }
