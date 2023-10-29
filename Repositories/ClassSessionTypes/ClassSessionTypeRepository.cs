645da00e3e8fc6418118b518a020c5a113921516
chuquan22 <81926487+chuquan22@users.noreply.github.com>
2023-10-30 02:14:07 +0700

    update

--- Repositories/ClassSessionTypes/ClassSessionTypeRepository.cs
+++ Repositories/ClassSessionTypes/ClassSessionTypeRepository.cs
@@ -11,6 +11,22 @@ namespace Repositories.ClassSessionTypes
     public class ClassSessionTypeRepository : IClassSessionTypeRepository
     {
         public ClassSessionTypesDAO db = new ClassSessionTypesDAO();
+
+        public bool CheckClassSessionTypeDuplicate(string name)
+        {
+            return db.CheckClassSessionTypeDuplicate(name);
+        }
+
+        public string CreateClassSessionType(ClassSessionType classSessionType)
+        {
+            return db.CreateClassSessionType(classSessionType);
+        }
+
+        public string DeleteClassSessionType(ClassSessionType classSessionType)
+        {
+            return db.UpdateClassSessionType(classSessionType);
+        }
+
         public ClassSessionType GetClassSessionType(int id)
         {
             return db.GetClassSessionType(id); 
@@ -19,12 +35,16 @@ namespace Repositories.ClassSessionTypes
         public ClassSessionType GetClassSessionTypeByName(string name)
         {
             return db.GetClassSessionTypeByName(name);
-
         }
 
         public List<ClassSessionType> GetListClassSessionType()
         {
             return db.GetListClassSessionType();
+        }
+
+        public string UpdateClassSessionType(ClassSessionType classSessionType)
+        {
+            return db.DeleteClassSessionType(classSessionType);
         }
     }
 }
