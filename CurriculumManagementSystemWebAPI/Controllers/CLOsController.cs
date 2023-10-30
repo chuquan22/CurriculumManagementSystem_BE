eae821eb65b8a38d967e1bf2abe8260bfc03038e
dmanhlam <dmanhlam3101@gmail.com>
2023-10-30 22:52:04 +0700

    fix clo session

--- CurriculumManagementSystemWebAPI/Controllers/CLOsController.cs
+++ CurriculumManagementSystemWebAPI/Controllers/CLOsController.cs
@@ -44,19 +44,13 @@ namespace CurriculumManagementSystemWebAPI.Controllers
             try
             {
                 CLO rs =  _mapper.Map<CLO>(clo);
-                var checkNameClo = repo.GetCLOByName(rs.CLO_name);
-                if(checkNameClo != null)
-                {
-                    return BadRequest(new BaseResponse(false, "CLOs Name already used in system.", rs));
-
-                }
                 rs = repo.CreateCLOs(rs);
                 return Ok(new BaseResponse(false, "Sucessfully", rs));
             }
-            catch (Exception)
+            catch (Exception ex)
             {
 
-                return BadRequest(new BaseResponse(true, "error", null));
+                return BadRequest(new BaseResponse(true,ex.Message, null));
             }
             return Ok(new BaseResponse(true, "False", null));
         }
