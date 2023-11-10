a76cb226b8fe1521bda7773c558c15866cdfee22
unknown <hoannnhe151308@fpt.edu.vn>
2023-11-10 14:36:32 +0700

    semesterplan

--- CurriculumManagementSystemWebAPI/Controllers/SemesterPlanController.cs
+++ CurriculumManagementSystemWebAPI/Controllers/SemesterPlanController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Repositories.SemesterPlans;
 using System.Runtime.Intrinsics.X86;
 using System.Security.Cryptography.Xml;
+using static Google.Apis.Requests.BatchRequest;
 
 namespace CurriculumManagementSystemWebAPI.Controllers
 {
@@ -37,7 +38,7 @@ namespace CurriculumManagementSystemWebAPI.Controllers
                     rs = _repo.GetSemesterPlanOverView(semester_id);
 
                 }
-                return Ok(new BaseResponse(false, "Successfully!", rs));
+                return Ok(new BaseResponse(false, "Create new semester plan successfully!", rs));
 
             }
             catch (Exception ex)
@@ -72,6 +73,62 @@ namespace CurriculumManagementSystemWebAPI.Controllers
             {
                 return BadRequest(new BaseResponse(true, "Error: " + ex.Message, null));
             }
+        }
+        [HttpGet("GetSemesterPlanDetails/{semester_id}")]
+        public ActionResult GetSemesterPlanDetails(int semester_id)
+        {
+            var semester_root = _context.Semester.Include(x => x.Batch).Where(x => x.semester_id == semester_id).FirstOrDefault();
+            int degreeLv = semester_root.degree_level_id;
+            var listSemesterPlan = _context.SemesterPlan
+               .Include(x => x.Curriculum)
+               .Include(x => x.Curriculum.Specialization)
+               .Include(x => x.Semester)
+               .Where(s => s.semester_id == semester_id)
+               .ToList();
+
+            var responseList = new List<SemesterPlanResponse>();
+
+            var uniqueCurriculumIds = listSemesterPlan.Select(sp => sp.Curriculum.specialization_id).Distinct().ToList();
+
+            foreach (var curriculumId in uniqueCurriculumIds)
+            {
+                var semesterPlanForCurriculum = listSemesterPlan.FirstOrDefault(sp => sp.Curriculum.specialization_id == curriculumId);
+
+                if (semesterPlanForCurriculum != null)
+                {
+                    var spe = semesterPlanForCurriculum.Curriculum.Specialization.specialization_english_name;
+                    var totalSemester = semesterPlanForCurriculum.Curriculum.total_semester;
+                    var semester = semesterPlanForCurriculum.Semester.semester_name + " " + semesterPlanForCurriculum.Semester.school_year;
+
+                    int order = semester_root.Batch.batch_order - semesterPlanForCurriculum.Curriculum.total_semester;
+                    var validSemester = _context.Semester.Include(x => x.Batch).Where(x => x.Batch.batch_order > order && x.Batch.batch_order <= semester_root.Batch.batch_order).Where(x => x.degree_level_id == degreeLv).OrderByDescending(x => x.Batch.batch_order).ToList();
+                    List<SemesterBatchResponse> batchResponses = new List<SemesterBatchResponse>();
+                    int i = 1;
+                    foreach (var item in validSemester)
+                    {
+                        SemesterBatchResponse batch = new SemesterBatchResponse();
+                        batch.semester_batch_id = item.semester_id;
+                        batch.semester_id = item.semester_id;
+                        batch.batch_id = item.Batch.batch_id;
+                        batch.batch_name = "K"+ item.Batch.batch_name;
+                        batch.term_no = i;
+                        batch.degree_level = 1;
+                        i++;
+                        batchResponses.Add(batch);
+                    }
+                    i = 1;
+                    var semesterPlanResponse = new SemesterPlanResponse
+                    {
+                        spe = spe,
+                        totalSemester = totalSemester,
+                        semester = semester,
+                        batch = batchResponses
+                    };
+
+                    responseList.Add(semesterPlanResponse);
+                }
+            }
+            return Ok(new BaseResponse(false, "123!", responseList));
         }
 
     }
