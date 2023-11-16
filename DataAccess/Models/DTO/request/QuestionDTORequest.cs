3fa32866fc884445f78bb9b3baa1ff93eb31950d
chuquan22 <81926487+chuquan22@users.noreply.github.com>
2023-11-16 12:02:55 +0700

    fix quiz

--- DataAccess/Models/DTO/request/QuestionDTORequest.cs
+++ DataAccess/Models/DTO/request/QuestionDTORequest.cs
@@ -14,10 +14,10 @@ namespace DataAccess.Models.DTO.request
         public string question_name { get; set; }
         public string question_type { get; set; }
         public int quiz_id { get; set; }
-        public string answers_1 { get; set; }
-        public string answers_2 { get; set; }
-        public string? answers_3 { get; set; }
-        public string? answers_4 { get; set; }
+        public string answers_A { get; set; }
+        public string answers_B { get; set; }
+        public string? answers_C { get; set; }
+        public string? answers_D { get; set; }
         public string correct_answer { get; set; }
     }
 }
